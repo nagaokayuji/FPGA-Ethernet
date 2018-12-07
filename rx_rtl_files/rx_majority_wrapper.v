2a750600e3693ac7f762584cfa560bc9b7ab60b8
y-nagaoka <qqxr4fe9k@gmail.com>
2018-12-07 16:52:23 +0900

    update startnewmeth

--- rx_rtl_files/rx_majority_wrapper.v
+++ rx_rtl_files/rx_majority_wrapper.v
@@ -2,7 +2,7 @@ module rx_majority_wrapper(
 	input wire clk,
 	input wire clk125MHz,
 	input wire reset,
-	input wire rx_clk,
+	input wire rx_clk, // main
 	input wire [7:0] rx_data,
 	input wire rx_enable,
 	output wire tmp,
@@ -46,8 +46,10 @@ wire rx_enable_seg[SEGMENT_NUM_MAX - 1: 0];
 wire [7:0] data_out_seg[SEGMENT_NUM_MAX - 1: 0];
 wire en_out_seg[SEGMENT_NUM_MAX - 1: 0];
 
-
+wire [7:0] data_out_seg_wire[SEGMENT_NUM_MAX - 1: 0];
+wire [8:0] en_and_data[SEGMENT_NUM_MAX - 1: 0];
 genvar i;
+
 generate
 for (i=0; i<SEGMENT_NUM_MAX; i=i+1) begin
 	rx_majority rx_majority_inst(
@@ -63,7 +65,38 @@ for (i=0; i<SEGMENT_NUM_MAX; i=i+1) begin
 		.en_out(en_out_seg[i]),
 		.data_out(data_out_seg[i])
 	);
+
+	assign data_out_seg_wire[i] = (en_out_seg[i] == 1'b1) ? data_out_seg[i] : 0;
+	assign en_and_data[i] = {en_out_seg[i],data_out_seg_wire[i]};
 end
 endgenerate
+/*****
+if (en[i]):
+	data = data[i];
+else if (en[i+1]):
+	data = data[i];
+
+*****************/
+
+reg [15:0] sel = 0;
+reg [7:0] data_out_seg_reg[SEGMENT_NUM_MAX - 1: 0];
+reg en_out_seg_reg[SEGMENT_NUM_MAX - 1: 0];
+reg [7:0] data_out_one;
+reg en_out_one;
+integer j;
+always @(posedge rx_clk) begin
+    for (j=0; j < SEGMENT_NUM_MAX; j=j+1) begin
+        if (en_out_seg[j]) begin
+            sel = j;
+        end
+        data_out_seg_reg[j] <= data_out_seg[j];
+        en_out_seg_reg[j] <= en_out_seg[j];
+    end
+    data_out_one <= data_out_seg_reg[sel];
+    en_out_one <= en_out_seg_reg[sel];
+end
+assign en_out = en_out_one;
+assign data_out = data_out_one;
+
 
 endmodule
